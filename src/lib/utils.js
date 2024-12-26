5560add14e3bab76f8503c878460fd7b4e540153
SravanthMolabanti <molabantisravanth@gmail.com>
2024-12-26 16:16:53 +0530

    upadted the origin five

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -10,7 +10,7 @@ export const generateToken=(userId,res)=>{
         maxAge:7*24*60*60*1000, //MS
         httpOnly:true, //prevents XSS
         sameSite:"strict",
-        secure:process.env.NODE_ENV!=="development"
+        secure:true,
     });
 
     return token;
