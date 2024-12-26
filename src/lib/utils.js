f77f6df7832e0853b91e8bb22e5fdcc00af211ae
SravanthMolabanti <molabantisravanth@gmail.com>
2024-12-26 16:29:23 +0530

    upadted the origin six

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken=(userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:7*24*60*60*1000, //MS
         httpOnly:true, //prevents XSS
-        sameSite:"strict",
+        sameSite:"none",
         secure:true,
     });
 
