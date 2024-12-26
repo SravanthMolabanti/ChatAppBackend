f5033db31899552cc40f3c99dbb74fe96619d6ca
SravanthMolabanti <molabantisravanth@gmail.com>
2024-12-26 15:34:00 +0530

    upadted the origin twice

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-    origin:true,
+    origin:"https://chat-app-frontend-eight-zeta.vercel.app/",
     credentials:true,
 }));
 
